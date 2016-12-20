c06fe4578eb546b7599882ba5cc28f3f98adc354
marakeby <marakeby@7e724e44-366f-48e5-b47a-921878d6225c>
2016-12-20 18:33:24 +0000

    bugfix #45, Page 12, Figure 9, this configuration is not possible (NOT connect to AND) in current editor for me.

--- Software/Code/branches/v1.1.0/Beacon/src/edu/vt/beacon/graph/glyph/arc/LogicArc.java
+++ Software/Code/branches/v1.1.0/Beacon/src/edu/vt/beacon/graph/glyph/arc/LogicArc.java
@@ -19,8 +19,29 @@ public class LogicArc extends AbstractArc {
         if (port == null || port.getParent() == null || port.getParent().getType() == null)
             return false;
 
-        return port.getParent().getType() == GlyphType.BIOLOGICAL_ACTIVITY;
+//        return port.getParent().getType() == GlyphType.BIOLOGICAL_ACTIVITY;
+        GlyphType type = port.getParent().getType();
 
+        if (type == GlyphType.BIOLOGICAL_ACTIVITY)
+            return true;
+
+        else if (type == GlyphType.PHENOTYPE || type == GlyphType.TAG || type == GlyphType.SUBMAP)
+            return false;
+
+//        Ther eis a contradiction in SBGN AF standard regarding the connection of logical arc (http://biecoll.ub.uni-bielefeld.de/volltexte/2015/5369/pdf/jib_265.pdf)
+//        section 3.3.1 includes an incidence matrix that shoes that logical arc can connect only biological activites to logical operators.
+//        section 2.8.5 says the logical arc can start from biological activity or any logical operator.
+        if (type == GlyphType.NOT || type == GlyphType.DELAY || type == GlyphType.AND || type == GlyphType.OR) {
+            for (int portIndex = 0; portIndex < port.getParent().getPortCount(); portIndex++)
+                for (int arcIndex = 0; arcIndex < port.getParent().getPortAt(portIndex).getArcCount(); arcIndex++)
+                    if (port.getParent().getPortAt(portIndex).getArcAt(arcIndex).getSourcePort() == port.getParent().getPortAt(portIndex) &&
+                            port.getParent().getPortAt(portIndex).getArcAt(arcIndex) != this &&
+                            port.getParent().getPortAt(portIndex).getArcAt(arcIndex) instanceof LogicArc)
+                        return false;
+
+            return true;
+        }
+        return false;
     }
 
     @Override
